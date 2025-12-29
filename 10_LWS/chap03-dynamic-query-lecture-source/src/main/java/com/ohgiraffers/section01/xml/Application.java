334cb74b88e9ce78c26fd6c7050c8637ed4eb16c
LimRaynor <morden9137@gmail.com>
2025-12-29 11:01:38 +0900

    Dynamic query

--- 10_LWS/chap03-dynamic-query-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ 10_LWS/chap03-dynamic-query-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -183,14 +183,14 @@ public class Application {
     private static Map<String, Object> inputChangeInfo() {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("변경할 메뉴 코드 입력 : ");
+        System.out.print("변경할 메뉴 코드 입력(!! 필수기재 !!) : ");
         int menuCode = sc.nextInt();
-        System.out.print("변경할 메뉴 이름 입력 : ");
+        System.out.print("변경할 메뉴 이름 입력(변경x -> 엔터) : ");
         sc.nextLine();
         String menuName = sc.nextLine();
-        System.out.print("변경할 카테고리 코드 입력 : ");
+        System.out.print("변경할 카테고리 코드 입력(변경x -> 0입력) : ");
         int categoryCode = sc.nextInt();
-        System.out.print("변경할 판매여부 입력 : ");
+        System.out.print("변경할 판매여부 입력(변경x -> 엔터) : ");
         sc.nextLine();
         String orderableStatus = sc.nextLine();
 
