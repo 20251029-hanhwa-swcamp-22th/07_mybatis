f3ef4e52f5d6c7174f3b39e8b6d5c0a95051c298
cyhhoo <cyhhoo@gmail.com>
2025-12-30 09:43:59 +0900

    251230 수업내용_최연후

---
+++ 15_Choiyeonhoo/chap06-transaction-lecture-source/src/main/java/com/mycompany/transaction/mapper/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.mycompany.transaction.mapper;
+
+import com.mycompany.transaction.domain.Order;
+import com.mycompany.transaction.domain.OrderMenu;
+import org.apache.ibatis.annotations.Mapper;
+
+
+// Mybatis가 해당 인터페이스를 구현하여 객체(proxy)로 만들고
+// 이를 Bean으로 등록
+@Mapper
+public interface OrderMapper {
+
+  void insertOrder(Order order);
+
+  void insertOrderMenu(OrderMenu orderMenu);
+
+}
