1cdadb8e7eead76370bb7e7ad9dcaf59abdbea30
jahyun725 <hwajh5229@gmail.com>
2025-12-30 17:31:43 +0900

    Revert "251230 JPA"
    This reverts commit 968a7d39a27f1665f4555006656d6581e5bb3c72.

--- 18_HwangJaHyun/chap06-transaction-lecture-source/src/main/java/com/my/transaction/service/OrderService.java
+++ 18_HwangJaHyun/chap06-transaction-lecture-source/src/main/java/com/my/transaction/service/OrderService.java
@@ -19,9 +19,6 @@ import java.util.stream.Collectors;
 public class OrderService {
   private final OrderMapper orderMapper;
 
-//  public OrderService(OrderMapper orderMapper) {
-//    this.orderMapper = orderMapper;
-//  }
   /**
    * 주문 등록 메서드
    * @param orderDTO
--- 18_HwangJaHyun/chap06-transaction-lecture-source/src/test/java/com/my/transaction/service/OrderServiceTest.java
+++ 18_HwangJaHyun/chap06-transaction-lecture-source/src/test/java/com/my/transaction/service/OrderServiceTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class OrderServiceTest {
+class OrderServiceIntegrationTest {
 
   @Autowired
   private OrderService orderService;
